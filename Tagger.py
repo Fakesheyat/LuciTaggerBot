cb31ab1cd1e3ad729dff6ec1cd8ef622bf927448
Fakesheyat <108465196+Fakesheyat@users.noreply.github.com>
2022-06-30 17:28:18 +0400

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -28,7 +28,7 @@ async def start(event):
                     buttons=(
                    
 		      [Button.url('Beni Gruba Ekle ➕', 'https://t.me/lucitaggerbot?startgroup=a')],
-                      [Button.url('Support🛠', 'https://t.me/LuciSup')],
+                      [Button.url('Support🛠', 'https://t.me/sohbet_qrupu_BT')],
                       [Button.url('Resmi Kanal📣', 'https://t.me/LuciBots')],
 		      [Button.url('Developer👨🏻‍💻', 'https://t.me/LuciMarka')],
                     ),
